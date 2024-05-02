762712ac5967136ffcec86c2404124c07368c085
Gabriel Lima <gabriellglma9@gmail.com>
2024-05-02 05:11:29 -0300

    Remoção de dado desnecessario

--- lib/app/model/movies_model.dart
+++ lib/app/model/movies_model.dart
@@ -10,17 +10,4 @@ class Movie {
     required this.posterPath,
     required this.overview,
   });
-
-  factory Movie.fromJson(Map<String, dynamic>? json) {
-    if (json == null) {
-      throw ArgumentError('json cannot be null');
-    }
-
-    return Movie(
-        id: json['id'] ?? 0, // Ou algum valor padrão se 'id' for nulo
-        title: json['title'] ?? '', // Ou algum valor padrão se 'title' for nulo
-        posterPath: json['poster_path'] ?? '',
-        overview: json['overview'] ?? '' // Ou algum valor padrão se 'poster_path' for nulo
-        );
-  }
 }
